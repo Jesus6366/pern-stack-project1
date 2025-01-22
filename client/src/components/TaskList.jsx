fdd77884ad0f284b969007fbb6613ebe5c20b1ed
Jesus jose martha laborin <polibos6366@gmail.com>
2025-01-22 14:08:10 -0700

    task list component done

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,8 +1,18 @@
-import { Button, Card, CardContent, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState([]);
+
+  const [editingTaskId, setEditingTaskId] = useState(null); // Task-specific editing state
+
   const loadTasks = async () => {
     // fetch data from server
     const response = await fetch("http://localhost:5000/api/tasks");
@@ -22,6 +32,38 @@ const TaskList = () => {
     loadTasks();
   };
 
+  const handleEdit = (task) => {
+    setEditingTaskId(task.id);
+    setTask(task);
+  };
+
+  const handleChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (id) => {
+    console.log(id);
+
+    console.log(task, "This is the data being send");
+
+    // PUT request to update data
+    const response = await fetch(`http://localhost:5000/api/tasks/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PUT",
+      body: JSON.stringify(task),
+    });
+    console.log(response);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    setEditingTaskId(null); // Exit editing mode
+    loadTasks();
+  };
+
   useEffect(() => {
     loadTasks();
   }, []);
@@ -29,9 +71,9 @@ const TaskList = () => {
   return (
     <>
       <h1>Task List</h1>
-      {tasks.map((task) => (
+      {tasks.map((t, id) => (
         <Card
-          key={task.title}
+          key={id}
           style={{
             marginBottom: ".7rem",
             backgroundColor: "#1e272e",
@@ -40,18 +82,54 @@ const TaskList = () => {
           <CardContent
             style={{ display: "flex", justifyContent: "space-between" }}
           >
-            <div style={{ color: "white" }}>
-              <Typography>{task.title}</Typography>
-              <Typography>{task.description}</Typography>
-            </div>
+            {editingTaskId === t.id ? (
+              <div>
+                <TextField
+                  hiddenLabel
+                  variant="filled"
+                  sx={{ display: "block", margin: ".5rem 0" }}
+                  inputProps={{ style: { color: "white" } }}
+                  InputLabelProps={{ style: { color: "white" } }}
+                  name="title"
+                  value={task.title || ""}
+                  onChange={handleChange}
+                />
+                <TextField
+                  variant="filled"
+                  sx={{ display: "block", margin: ".5rem 0" }}
+                  inputProps={{ style: { color: "white" } }}
+                  InputLabelProps={{ style: { color: "white" } }}
+                  name="description"
+                  value={task.description || ""}
+                  onChange={handleChange}
+                />
+              </div>
+            ) : (
+              <div style={{ color: "white" }}>
+                <Typography>{t.title}</Typography>
+                <Typography>{t.description}</Typography>
+              </div>
+            )}
+
             <div>
-              <Button
-                variant="contained"
-                color="inherit"
-                onClick={() => console.log("Editing")}
-              >
-                Edit
-              </Button>
+              {editingTaskId === t.id ? (
+                <Button
+                  variant="contained"
+                  color="inherit"
+                  onClick={() => handleSubmit(t.id)}
+                >
+                  Save
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  color="inherit"
+                  onClick={() => handleEdit(t)}
+                >
+                  Edit
+                </Button>
+              )}
+
               <Button
                 variant="contained"
                 color="warning"
